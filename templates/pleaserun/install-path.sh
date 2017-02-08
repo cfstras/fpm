91507eb20622616bef66c4b9ec726f157e918f97
Claus Strasburger <claus@strasburger.de>
2017-02-08 16:16:09 +0100

    pleaserun: nicer formatting for comment lines in config

--- templates/pleaserun/install-path.sh
+++ templates/pleaserun/install-path.sh
@@ -12,17 +12,21 @@ install_path() {
     fi
     if [ -f "$1" ] ; then
       _old_file="$d"
-      if [ "$(dirname "$d")" = "/etc/default" ] && [ -f "${_old_file}" ]; then
+      if [ "$(dirname "$d")" = "/etc/default" ]; then
         # upgrade
         echo "upgrading file $1"
         _new_entries=$(awk -F"=" '{print $1}' "$1")
-        _old_entries=$(awk -F"=" '{print $1}' "${_old_file}")
+        if [ -f "${_old_file}" ]; then
+          _old_entries=$(awk -F"=" '{print $1}' "${_old_file}")
+        else
+          _old_entries=""
+        fi
         IFS='
 '
         for key in ${_new_entries}; do
           if ! echo "${_old_entries}" | silent grep "${key}"; then
-            echo "New setting: $(egrep "^${key}=" "$1")"
-            egrep "^${key}" "$1" >> "${_old_file}"
+            [ -f "${_old_file}" ] && echo "New setting: $(egrep "^${key}=" "$1")"
+            egrep "^${key}" "$1" | sed 's|^export\s*#|#|g' | sed 's|^\s*\(#.*\)=""|\1|g' >> "${_old_file}"
           fi
         done
         # TODO obsolete settings
